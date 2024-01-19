c2e0c13095db1f5a6593b609472ed7863d60b92b
Hiasen <howon5@naver.com>
2024-01-19 17:19:31 +0900

    2024-01-19-cp
    enum study
    회사 일 바빠서 못함

--- example/7_union-intersection.ts
+++ example/7_union-intersection.ts
@@ -1,40 +1,41 @@
-function logMessage(value: string) {
-  console.log(value);
-}
-function logMessage(value: number) {
-  console.log(value);
-}
-function logMessage(value: any) {
-  console.log(value);
-}
+// function LogMessage(value: any): void {
+//   console.log(value);
+// }
+// LogMessage('Hello');
+// LogMessage(false);
 
-// # Union 타입 문법 - `any` 보다는 명시적임
+var seho: string | number | boolean;
 function logMessage(value: string | number) {
-  console.log(value);
-}
-
-function logMessage(value: string | number) {
-  if (typeof value === 'string') {
-    value.toLocaleUpperCase();
-  }
-  if (typeof value === 'number') {
+  if (typeof value == 'number') {
     value.toLocaleString();
   }
-  throw new TypeError('value must be string or number')
+  if (typeof value == 'string') {
+    value.toString();
+  }
+  throw new TypeError('value must be string or number');
 }
+logMessage("");
+logMessage(1);
 
-// # Intersection 타입 문법
 interface Developer {
   name: string;
   skill: string;
 }
-
 interface Person {
   name: string;
   age: number;
 }
 
 function askSomeone(someone: Developer | Person) {
-  someone.name; // O
-  someone.age; // X
+  // someone.name
 }
+askSomeone( {name: '디벨로퍼', skill: '웹 개발'} );
+askSomeone( {name: '캡틴', age: 100} );
+
+function askSomeone2(someone: Developer & Person) {
+  someone.name
+  someone.age
+  someone.skill
+}
+
+askSomeone2({name: '디벨로퍼', skill: '웹 개발', age: 100});
