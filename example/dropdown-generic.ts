c4008798af38c476e309339eaa980a5483a865ee
Hiasen <howon5@naver.com>
2024-01-24 17:09:34 +0900

    2024-01-24-cp

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,26 +1,39 @@
-const emails = [
+interface Email<T>{
+  value: T;
+  selected: boolean;
+}
+
+const emails: Email<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+// interface ProductNumber{
+//   value: number;
+//   selected: boolean;
+// }
+
+const numberOfProducts: Email<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem<T>(item: Email<T>) {
   const option = document.createElement('option');
-  option.value = item.value.toString();
-  option.innerText = item.value.toString();
+  if(item.value){
+    option.value = item.value.toString();
+    option.innerText = item.value.toString();
+  }
   option.selected = item.selected;
   return option;
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
+
