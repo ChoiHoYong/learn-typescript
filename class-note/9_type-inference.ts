f4cd71d12de8f4ffa6265bb8f3836f34e5848896
Hiasen <howon5@naver.com>
2024-01-29 15:39:04 +0900

    2024-01-29-cp
    타입 추론 마무리

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -1,19 +1,42 @@
 // 타입 추론 기본 1
-var a = 10;
-var b = '10';
+// var a = 10;
+// var b = '10';
 
-function getC(c = 10) {
-    var d = 'hi';
-    return c + d;
-}
+// function getC(c = 10) {
+//     var d = 'hi';
+//     return c + d;
+// }
 
 // 타입 추론 기본 2
+// interface Dropdown<T> {
+//     value: T;
+//     title: string;
+// }
+
+// var shoppingItem: Dropdown<string> = {
+//     value: 'abc',
+//     title: 'def',
+// }
+
+// var ef = shoppingItem;
+
+// 타입 추론 기본 3
 interface Dropdown<T> {
     value: T;
     title: string;
 }
 
-var shoppingItem: Dropdown<string> = {
-    value: 'abc',
-    title: 'def',
+interface DetailedDropdown<K> extends Dropdown<K>{
+    description: string;
+    tag: K;
 }
+
+var DetailedItem: DetailedDropdown<string> = {
+    title: 'abc',
+    description: 'ab',
+    value: 'bc',
+    tag: 'cd',
+}
+
+// Best Common Type
+var arr = [1,2,true];
